9c39f9f4de6c022c191025cc5e633ef1bbed7a53
Lijun Tang <hercule2400@gmail.com>
2015-11-08 22:07:41 -0600

    Added 3 queues for priority and initialized conditional variable

--- p3/thread_pool.c
+++ p3/thread_pool.c
@@ -20,6 +20,9 @@
 
 #define MAX_THREADS 4
 #define STANDBY_SIZE 10
+#define FIRST 1
+#define BIZ 2
+#define COACH 3
 
 // element to be added to the queue
 typedef struct pool_task {
@@ -32,7 +35,14 @@ struct pool_t {
   pthread_mutex_t lock;
   pthread_cond_t notify;
   pthread_t *threads;
-  pool_task_t *queue;
+  pool_task_t *parse_queue_head;
+  pool_task_t *parse_queue_tail;
+  pool_task_t *first_queue_head;
+  pool_task_t *first_queue_tail;
+  pool_task_t *biz_queue_head;
+  pool_task_t *biz_queue_tail;
+  pool_task_t *coach_queue_head;
+  pool_task_t *coach_queue_tail;
   int thread_count;
   int task_queue_size_limit;
 };
@@ -55,7 +65,15 @@ pool_t *pool_create(int queue_size, int num_threads)
         }
     }
     pool_t *pool = (pool_t *) malloc(sizeof(pool_t));
-    pool->queue = NULL;
+    pthread_cond_init(&(pool->notify), NULL);
+    pool->parse_queue_head = NULL;
+    pool->parse_queue_tail = NULL;
+    pool->first_queue_head = NULL;
+    pool->first_queue_tail = NULL;
+    pool->biz_queue_head = NULL;
+    pool->biz_queue_tail = NULL;
+    pool->coach_queue_head = NULL;
+    pool->coach_queue_tail = NULL;
     pool->thread_count = MAX_THREADS;
     pool->task_queue_size_limit = queue_size;
     return pool;
@@ -68,9 +86,53 @@ pool_t *pool_create(int queue_size, int num_threads)
  */
 int pool_add_task(pool_t *pool, void* (*function)(void *), void *argument)
 {
-    int err = 0;
+    argument_t *arguments = (argument_t *) argument;
+    pool_task_t *task = (pool_task_t *) malloc(sizeof(pool_task_t));
+    task->argument = argument;
+    task->next = NULL;
+
+    if (arguments->status == PARSE) {
+        if (pool->parse_queue_head == NULL) {
+            pool->parse_queue_head = task;
+            pool->parse_queue_tail = task;
+        } else {
+            pool->parse_queue_tail->next = task;
+            pool->parse_queue_tail = task;
+        }
+    }
+
+    if (arguments->status == PROCESS) {
+        struct request *req = arguments->req;
+        if (req->customer_priority == FIRST) {
+            if (pool->first_queue_head == NULL) {
+                pool->first_queue_head = task;
+                pool->first_queue_tail = task;
+            } else {
+                pool->first_queue_tail->next = task;
+                pool->parse_queue_tail = task;
+            }
+        }
+        if (req->customer_priority == BIZ) {
+            if (pool->biz_queue_head == NULL) {
+                pool->biz_queue_head = task;
+                pool->biz_queue_tail = task;
+            } else {
+                pool->biz_queue_tail->next = task;
+                pool->biz_queue_tail = task;
+            }
+        }
+        if (req->customer_priority == COACH) {
+            if (pool->coach_queue_head == NULL) {
+                pool->coach_queue_head = task;
+                pool->coach_queue_tail = task;
+            } else {
+                pool->coach_queue_tail->next = task;
+                pool->coach_queue_tail = task;
+            }
+        }
+    }
         
-    return err;
+    return 0;
 }
 
 
@@ -83,6 +145,7 @@ int pool_destroy(pool_t *pool)
 {
     int err = 0;
  
+    pthread_cond_destroy(&(pool->notify));
     free(pool);
     return err;
 }
@@ -113,5 +176,6 @@ void* handler(void *arg)
     if (arguments->status == PROCESS) {
         process_request(arguments->connfd, arguments->req); 
     }
+    free(arguments);
     pthread_exit(NULL);
 }
